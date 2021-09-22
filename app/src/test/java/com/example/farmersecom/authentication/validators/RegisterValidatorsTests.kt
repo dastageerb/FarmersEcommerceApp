2daa72f0b020bc5611c9ed0077520cdd878042f6
dastageerb <[dastaggeerb44@gmail.com]>
2021-09-21 23:59:34 -0700

    login validation tests

---
+++ app/src/test/java/com/example/farmersecom/authentication/validators/RegisterValidatorsTests.kt
@@ -0,0 +1,16 @@
+package com.example.farmersecom.authentication.validators
+
+import com.google.common.truth.Truth.assertThat
+import com.wajahatkarim3.easyvalidation.core.view_ktx.nonEmpty
+import com.wajahatkarim3.easyvalidation.core.view_ktx.validator
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.junit.runners.JUnit4
+
+@RunWith(JUnit4::class)
+class RegisterValidatorsTests
+{
+
+
+
+}
